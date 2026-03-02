e8196410a643586f583135f85dc8a1303f238b81
sfinksik12 <vasya1pupkin12453@gmail.com>
2026-03-02 18:57:27 +0300

    удалено api

--- tests/api.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '../index';
-import { OrderService } from '../src/api/orderService';
-
-test('Get all orders', async ({ request }) => {
-  const orderService = new OrderService(request);
-  const response = await orderService.getPage();
-  console.log(response);
-});
